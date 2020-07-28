a96717f922ceef1575eba4e60de2e0cc093e68ef
Sean Newman <sxn6296@rit.edu>
2020-07-27 20:20:07 -0400

    Remove git completion

--- install.sh
+++ install.sh
@@ -23,8 +23,5 @@ for config in ${configs[*]}; do
 	ln -fs ${CONFIGDIR}/${config} ${HOME}/${config}
 done
 
-# Download bash completion file
-curl -o ${HOME}/.git-completion.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
-
 # Install vim plugins
 echo -ne '\n' | vim +PlugInstall +qall
